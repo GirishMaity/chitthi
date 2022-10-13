e0b69924d02c07dc1b75d83fed048b3557f11bc1
Lakshay Kumar <2001Lakshaygdkumar02@gmail.com>
2022-10-13 14:18:27 +0530

    All Chitthi files

---
+++ frontend/src/components/ChatLoading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ChatLoading.css'
+
+const ChatLoading = () => {
+  return (
+    <div className='box'>
+
+    <div class="lds-facebook"><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default ChatLoading
